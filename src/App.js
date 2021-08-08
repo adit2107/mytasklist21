e239f0e27f8a572039988ebcc2d8c30941a42b23
adit2107 <adit.ratnam@gmail.com>
2021-08-08 11:40:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import TopbarComponent from './shared/topbar.component';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React, { useEffect } from 'react';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import AllRoutes from './routes/routes';
 import RegisterComponent from './components/user/register.component';
 
 function App() {
+
+  useEffect(() => {
+    document.title = `My tasklist`;
+  }, [])
+  
   return (
     <div className="App">
    <Provider store={store}>
